822d9493aaaf9d228b1137d64eeb64df5b951d9e
PoliZyh <272798802@qq.com>
2023-10-03 18:51:11 +0800

    feat: 完成spu模块所有业务

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -1,87 +1,123 @@
-import request from "@/utils/request";
+import request from '@/utils/request'
 import type {
-    I_SPU_LIST_RESPONSE_DATA,
-    I_ALL_TRADEMARK_RESPONSE_DATA,
-    I_SPU_IMGAGE_LIST_RESPONSE_DATA,
-    I_SALE_ATTR_RESPONSE_DATA,
-    I_ALL_SALE_ATTR_RESPONSE_DATA,
-    I_SPU_OBJ
+  I_SPU_LIST_RESPONSE_DATA,
+  I_ALL_TRADEMARK_RESPONSE_DATA,
+  I_SPU_IMGAGE_LIST_RESPONSE_DATA,
+  I_SALE_ATTR_RESPONSE_DATA,
+  I_ALL_SALE_ATTR_RESPONSE_DATA,
+  I_SPU_OBJ,
+  I_SKU_DATA,
+  I_SKU_INFO_RESPONSE_DATA
 } from './type'
 
 enum API {
-    // 获取spu列表接口地址
-    GET_SPU_LIST_URL = '/admin/product',
-    // 获取全部品牌的数据地址
-    GET_ALL_TRADEMARK_URL = '/admin/product/baseTrademark/getTrademarkList',
-    // 获取某一个SPU下的照片墙数据地址
-    GET_SPU_IMAGE_LIST_URL = '/admin/product/spuImageList',
-    // 获取某一个SPU下全部的已有的销售属性接口地址
-    GET_SPU_SALE_ATTR_URL = '/admin/product/spuSaleAttrList',
-    // 获取整个项目全部的销售属性[颜色、版本、尺码]
-    GET_ALL_SALE_ATTR_URL = '/admin/product/baseSaleAttrList',
-    // 添加一个新的SPU
-    ADD_SPU_URL = '/admin/product/saveSpuInfo',
-    // 更新某一个SPU
-    UPDATE_SPU_URL = '/admin/product/updateSpuInfo'
+  // 获取spu列表接口地址
+  GET_SPU_LIST_URL = '/admin/product',
+  // 获取全部品牌的数据地址
+  GET_ALL_TRADEMARK_URL = '/admin/product/baseTrademark/getTrademarkList',
+  // 获取某一个SPU下的照片墙数据地址
+  GET_SPU_IMAGE_LIST_URL = '/admin/product/spuImageList',
+  // 获取某一个SPU下全部的已有的销售属性接口地址
+  GET_SPU_SALE_ATTR_URL = '/admin/product/spuSaleAttrList',
+  // 获取整个项目全部的销售属性[颜色、版本、尺码]
+  GET_ALL_SALE_ATTR_URL = '/admin/product/baseSaleAttrList',
+  // 添加一个新的SPU
+  ADD_SPU_URL = '/admin/product/saveSpuInfo',
+  // 更新某一个SPU
+  UPDATE_SPU_URL = '/admin/product/updateSpuInfo',
+  // 追加一个新的SKU地址
+  ADD_SKU_URL = '/admin/product/saveSkuInfo',
+  // 查看某一个已有的SPU下全部售卖的商品
+  GET_INFO_URL = '/admin/product/findBySpuId',
+  // 删除已有的SPU
+  DELETE_SPU_URL = '/admin/product/deleteSpu'
 }
 
-
 // 获取spu列表接口方法
-export const reqGetSpuList = (page: number, limit: number, category3Id: number) => {
-    return request<any, I_SPU_LIST_RESPONSE_DATA>({
-        url: API.GET_SPU_LIST_URL + `/${page}/${limit}?category3Id=${category3Id}`,
-        method: 'get',
-    })
+export const reqGetSpuList = (
+  page: number,
+  limit: number,
+  category3Id: number,
+) => {
+  return request<any, I_SPU_LIST_RESPONSE_DATA>({
+    url: API.GET_SPU_LIST_URL + `/${page}/${limit}?category3Id=${category3Id}`,
+    method: 'get',
+  })
 }
 
 // 获取全部spu品牌的数据
 export const reqGetAllTrademark = () => {
-    return request<any, I_ALL_TRADEMARK_RESPONSE_DATA>({
-        url: API.GET_ALL_TRADEMARK_URL,
-        method: 'get'
-    })
+  return request<any, I_ALL_TRADEMARK_RESPONSE_DATA>({
+    url: API.GET_ALL_TRADEMARK_URL,
+    method: 'get',
+  })
 }
 
 // 获取某一个SPU照片墙接口
 export const reqGetSpuImageList = (spuId: number) => {
-    return request<any, I_SPU_IMGAGE_LIST_RESPONSE_DATA>({
-        url: API.GET_SPU_IMAGE_LIST_URL + `/${spuId}`,
-        method: 'get'
-    })
+  return request<any, I_SPU_IMGAGE_LIST_RESPONSE_DATA>({
+    url: API.GET_SPU_IMAGE_LIST_URL + `/${spuId}`,
+    method: 'get',
+  })
 }
 
 // 获取某一个已有的SPU拥有的销售属性
 export const reqGetSpuSaleAttr = (spuId: number) => {
-    return request<any, I_SALE_ATTR_RESPONSE_DATA>({
-        url: API.GET_SPU_SALE_ATTR_URL + `/${spuId}`,
-        method: 'get'
-    })
+  return request<any, I_SALE_ATTR_RESPONSE_DATA>({
+    url: API.GET_SPU_SALE_ATTR_URL + `/${spuId}`,
+    method: 'get',
+  })
 }
 
 // 获取项目全部的销售属性
 export const reqGetAllSaleAttr = () => {
-    return request<any, I_ALL_SALE_ATTR_RESPONSE_DATA>({
-        url: API.GET_ALL_SALE_ATTR_URL,
-        method: 'get'
-    })
+  return request<any, I_ALL_SALE_ATTR_RESPONSE_DATA>({
+    url: API.GET_ALL_SALE_ATTR_URL,
+    method: 'get',
+  })
 }
 
 // 添加一个新的SPU
 // data为新增或已有的SPU字段
 export const reqAddOrUpdateSpu = (data: I_SPU_OBJ) => {
-    if (data.id) {
-        // 若有id则更新
-        return request<any, any>({
-            url: API.UPDATE_SPU_URL,
-            method: 'post',
-            data
-        })
-    } else {
-        // 没有id则为添加
-        return request<any, any>({
-            url: API.ADD_SPU_URL,
-            method: 'post',
-            data
-        })
-    }
+  if (data.id) {
+    // 若有id则更新
+    return request<any, any>({
+      url: API.UPDATE_SPU_URL,
+      method: 'post',
+      data,
+    })
+  } else {
+    // 没有id则为添加
+    return request<any, any>({
+      url: API.ADD_SPU_URL,
+      method: 'post',
+      data,
+    })
+  }
+}
+
+// 新增sku请求的方法
+export const reqAddSku = (data: I_SKU_DATA) => {
+  return request<any, any>({
+    url: API.ADD_SKU_URL,
+    method: 'post',
+    data
+  })
+}
+
+// 获取spu下所有商品的方法
+export const reqGetSkuList = (spuId: number) => {
+  return request<any, I_SKU_INFO_RESPONSE_DATA>({
+    url: API.GET_INFO_URL + `/${spuId}`,
+    method: 'get'
+  })
+}
+
+// 删除已有的SPU的方法
+export const reqDeleteSpu = (spuId: number) => {
+  return request<any, any>({
+    url: API.DELETE_SPU_URL + `/${spuId}`,
+    method: 'delete'
+  })
 }
--- src/views/Product/Spu/index.vue
+++ src/views/Product/Spu/index.vue
@@ -1,44 +1,98 @@
 <template>
   <div>
     <Category :disabled="isDisabled"></Category>
-    <el-card style="margin: 10px 0;">
+    <el-card style="margin: 10px 0">
       <!-- 场景--展示table -->
       <div v-show="scene === 0">
-        <el-button type="primary" icon="Plus" :disabled="!categoryStore.c3Id" @click="handleAddSpu">添加SPU</el-button>
+        <el-button
+          type="primary"
+          icon="Plus"
+          :disabled="!categoryStore.c3Id"
+          @click="handleAddSpu"
+        >
+          添加SPU
+        </el-button>
         <!-- table -->
-        <el-table style="margin: 10px 0;" border :data="spuList">
-          <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+        <el-table style="margin: 10px 0" border :data="spuList">
+          <el-table-column
+            label="序号"
+            type="index"
+            align="center"
+            width="80px"
+          ></el-table-column>
           <el-table-column label="SPU名称" prop="spuName"></el-table-column>
-          <el-table-column label="SPU描述" prop="description" show-overflow-tooltip></el-table-column>
+          <el-table-column
+            label="SPU描述"
+            prop="description"
+            show-overflow-tooltip
+          ></el-table-column>
           <el-table-column label="操作">
-            <template #="{ row, $index }">
-              <el-button size="small" icon="Plus" title="添加SKU"></el-button>
-              <el-button size="small" icon="Edit" title="修改SPU" @click="handleUpdateSpu(row)"></el-button>
-              <el-button size="small" icon="View" title="查看SKU列表"></el-button>
-              <el-button size="small" icon="Delete" title="删除SPU"></el-button>
+            <template #="{ row }">
+              <el-button size="small" icon="Plus" title="添加SKU" @click="handleAddSku(row)"></el-button>
+              <el-button
+                size="small"
+                icon="Edit"
+                title="修改SPU"
+                @click="handleUpdateSpu(row)"
+              ></el-button>
+              <el-button
+                size="small"
+                icon="View"
+                title="查看SKU列表"
+                @click="handleFindSku(row)"
+              ></el-button>
+              <el-popconfirm title="你确定删除该SPU嘛?" @confirm="handleDeleteSpu(row)">
+                <template #reference>
+                  <el-button size="small" icon="Delete" title="删除SPU"></el-button>
+                </template>
+              </el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
         <!-- 分页器 -->
-        <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[3, 5, 7, 9]"
-          :background="true" layout="prev, pager, next, jumper, ->, sizes, total" :total="totalSpu"
-          @size-change="handleSizeChange" @current-change="getSpuList" />
+        <el-pagination
+          v-model:current-page="pageNo"
+          v-model:page-size="pageSize"
+          :page-sizes="[3, 5, 7, 9]"
+          :background="true"
+          layout="prev, pager, next, jumper, ->, sizes, total"
+          :total="totalSpu"
+          @size-change="handleSizeChange"
+          @current-change="getSpuList"
+        />
       </div>
       <!-- 场景--添加修改Spu -->
-      <SpuFrom ref="spuFormRef" v-show="scene === 1" @change-scene="changeScene"></SpuFrom>
+      <SpuFrom
+        ref="spuFormRef"
+        v-show="scene === 1"
+        @change-scene="changeScene"
+      ></SpuFrom>
       <!-- 场景--添加Sku -->
-      <SkuForm v-show="scene === 2"></SkuForm>
+      <SkuForm v-show="scene === 2" @change-scene="changeScene" ref="skuFormRef"></SkuForm>
     </el-card>
+    <el-dialog title="SKU列表" v-model="isShowDialog">
+      <el-table border :data="skuList">
+        <el-table-column label="sku名字" prop="skuName"></el-table-column>
+        <el-table-column label="sku价格" prop="price"></el-table-column>
+        <el-table-column label="sku重量" prop="weight"></el-table-column>
+        <el-table-column label="sku图片">
+          <template #="{ row }">
+            <img :src="row.skuDefaultImg" style="width: 100px;height: 100px;">
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import useCategoryStore from '@/store/modules/useCategoryStore';
-import { reqGetSpuList } from '@/api/product/spu';
-import type { I_SPU_LIST, I_SPU_OBJ } from '@/api/product/spu/type';
+import { ref, watch, onBeforeUnmount } from 'vue'
+import useCategoryStore from '@/store/modules/useCategoryStore'
+import { reqGetSpuList, reqGetSkuList, reqDeleteSpu } from '@/api/product/spu'
+import type { I_SKU_DATA, I_SPU_LIST, I_SPU_OBJ } from '@/api/product/spu/type'
 import SpuFrom from './SpuForm.vue'
 import SkuForm from './SkuForm.vue'
+import { ElMessage } from 'element-plus'
 
 const isDisabled = ref<boolean>(false)
 const categoryStore = useCategoryStore()
@@ -47,7 +101,10 @@ let pageSize = ref<number>(3)
 let spuList = ref<I_SPU_LIST>([])
 let totalSpu = ref<number>(0)
 let scene = ref<0 | 1 | 2>(0) // 0-展示table 1-添加修改Spu 2-添加Sku
+let skuList = ref<I_SKU_DATA[]>()
+let isShowDialog = ref<boolean>(false)
 const spuFormRef = ref<any>() // SpuForm组件实例
+const skuFormRef = ref<any>() // SkuForm组件实例
 
 // 监听三级菜单的变化
 watch(
@@ -55,20 +112,28 @@ watch(
   () => {
     if (!categoryStore.c3Id) return
     getSpuList()
-  }
+  },
 )
+
+onBeforeUnmount(() => {
+  categoryStore.$reset()
+})
 
 // 获取三级分类下的SPU
 const getSpuList = async (pager: number = 1) => {
   pageNo.value = pager
-  const res = await reqGetSpuList(pageNo.value, pageSize.value, categoryStore.c3Id as number)
+  const res = await reqGetSpuList(
+    pageNo.value,
+    pageSize.value,
+    categoryStore.c3Id as number,
+  )
   if (res.code === 200) {
     spuList.value = res.data.records
     totalSpu.value = res.data.total
   }
 }
 
-// 页大小变化 
+// 页大小变化
 const handleSizeChange = () => {
   if (!categoryStore.c3Id) return
   getSpuList()
@@ -93,6 +158,37 @@ const handleUpdateSpu = (spuItem: I_SPU_OBJ) => {
   scene.value = 1
   // 调用自组件实例方法获取spu数据
   spuFormRef.value.getSpuData(spuItem)
+}
+
+// 添加sku
+const handleAddSku = (spuItem: I_SPU_OBJ) => {
+  // 切换场景 -> skuForm
+  scene.value = 2
+  // 调用子组件获取sku数据
+  const { c1Id, c2Id } = categoryStore
+  skuFormRef.value.getSkuData(c1Id as number, c2Id as number, spuItem)
+}
+
+// 展示sku
+const handleFindSku = async (row: I_SPU_OBJ) => {
+  const res = await reqGetSkuList(row.id as number)
+  if (res.code === 200) {
+    skuList.value = res.data
+    isShowDialog.value = true
+  } else {
+    ElMessage.error('获取skuList失败')
+  }
+}
+
+// 删除spu
+const handleDeleteSpu = async (row: I_SPU_OBJ) => {
+  const res = await reqDeleteSpu(row.id as number)
+  if (res.code === 200) {
+    ElMessage.success('删除成功')
+    await getSpuList()
+  } else {
+    ElMessage.error('删除失败')
+  }
 }
 </script>
 
